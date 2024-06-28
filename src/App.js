54a28f493b7d59e2b874684cfde199ce847cf762
Nicolas Santos <nicolassilva15br@gmail.com>
2024-06-28 11:08:21 -0300

    Primeiro commit: adicionando arquivos iniciais

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bem-vindo à minha aplicação React!</h1>
+        <p>Esta é uma aplicação simples criada com create-react-app.</p>
       </header>
     </div>
   );
